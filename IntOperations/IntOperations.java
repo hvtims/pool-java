7a733b4bab01bc211553a5eeb320884548a7e701
hvtims <timo.goog03@gmail.com>
2025-11-03 11:02:49 +0100

    interopretions

---
+++ IntOperations/IntOperations.java
@@ -0,0 +1,14 @@
+public class IntOperations {
+    public static int addTwoIntegers(int a, int b) {
+        return a + b ;
+    }
+    public static int subtractTwoIntegers(int a, int b) {
+       return b - a ;
+    }
+    public static int multiplyTwoIntegers(int a, int b) {
+        return a * b;
+    }
+    public static int divideTwoIntegers(int a, int b) {
+        return a / b;
+    }
+}
