bb5c047d1e76ed8d5c81e4ae8e019148493aa3f4
hvtims <timo.goog03@gmail.com>
2025-11-03 11:32:04 +0100

    stringcocncat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -2,6 +2,8 @@ public class StringConcat {
     public static String concat(String s1, String s2) {
         if (s1 == null){
             return s2;
+        }else if (s2 == null){
+            return s1;
         }
             return s1.concat(s2);
     }
