203384b33b3f28ec19a028f0ec786da738004c14
hvtims <timo.goog03@gmail.com>
2025-11-03 11:17:13 +0100

    chifoumi

---
+++ Chifoumi/Chifoumi.java
@@ -0,0 +1,10 @@
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        if (chifoumiAction == ChifoumiAction.ROCK){
+            return ChifoumiAction.SCISSOR;
+        }else if (chifoumiAction == ChifoumiAction.PAPER){
+            return ChifoumiAction.ROCK;
+        }
+        return ChifoumiAction.PAPER;
+    }
+}
