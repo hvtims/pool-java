8d555c3b20c31a5b6b2c8c810a5d6068c568ae4a
hvtims <timo.goog03@gmail.com>
2025-11-04 11:58:54 +0100

    capiize

--- Capitalize/ExerciseRunner.java
+++ Capitalize/ExerciseRunner.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 public class ExerciseRunner {
     public static void main(String[] args) throws IOException {
         Capitalize.capitalize(new String[]{"input", "output"});
-        String expectedResult = new String(Files.readAllBytes(Paths.get("result")));
+        String expectedResult = new String(Files.readAllBytes(Paths.get("input")));
         String userOutput = new String(Files.readAllBytes(Paths.get("output")));
         System.out.println(expectedResult.equals(userOutput));
     }
