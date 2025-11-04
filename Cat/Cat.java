f573e750241a46076f9f1e4f319b397991a5690e
hvtims <timo.goog03@gmail.com>
2025-11-04 11:46:08 +0100

    cat

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -1,13 +1,18 @@
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.IOException; 
+import java.nio.file.Path;
+
 public class Cat {
     public static void cat(String[] args) throws IOException {
-        if (args.length == 0){
-            return;
+        if (args.length==0){
+            return ;
         }
-        String content = Files.readString(Paths.get(args[0]));
-        System.out.println(content);
+         Path filePath = Path.of(args[0]);
+        try {
+           byte[] content = Files.readAllBytes(filePath);
+            System.out.write(content);
+        } catch (IOException e) {
+            System.err.println( e.getMessage());
+        }
     }
 }
