ce5cfbfbdd91d4b39c046715004dd21426965ca5
hvtims <timo.goog03@gmail.com>
2025-11-06 11:06:57 +0100

    starcp,srtop,

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,19 @@
+public class CelestialObject {
+    public double x ;
+    public double y;
+    public double z;
+    public String name;
+
+    public CelestialObject(){
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+    public  CelestialObject(String namee double xx, double yy, double zz){
+        this.name = namee ;
+        this.x = xx ;
+        this.y = yy;
+        this.z = zz; 
+    }
+}
