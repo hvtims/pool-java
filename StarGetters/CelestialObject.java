eac29cd70d1ae797787a43e88fce57f42a42624a
hvtims <timo.goog03@gmail.com>
2025-11-06 11:15:25 +0100

    sargetters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,42 @@
+public class CelestialObject {
+    public double x ;
+    public double y;
+    public double z;
+    public String name;
+    public void setX(double xx){
+        this.x = xx ;
+    }
+    public void setY (double yy){
+        this.x = yy;
+    }
+    public void setZ(double zz){
+        this.z = zz;
+    }
+    public void setName(String namee){
+        this.name = namee;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public double getX(){
+        return this.x;
+    }
+    public double getY(){
+        return this.y;
+    }
+    public double getZ(){
+        return this.z;
+    }
+    public CelestialObject(){
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+    public  CelestialObject(String namee ,double xx, double yy, double zz){
+        this.name = namee ;
+        this.x = xx ;
+        this.y = yy;
+        this.z = zz; 
+    }
+}
