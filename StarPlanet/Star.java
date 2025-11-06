3703e96743966ed4035b61ebc618e9c5b046cb93
hvtims <timo.goog03@gmail.com>
2025-11-06 12:17:51 +0100

    star

--- StarPlanet/Star.java
+++ StarPlanet/Star.java
@@ -1,41 +1,38 @@
+import java.util.Objects;
 
-public class Star extends CelestialObject {
-
+public class Star extends CelestialObject{
     private double magnitude;
 
-    public double getMagnitude() {
-        return this.magnitude;
-    }
-
-    public void setMagnitude(double test) {
-        this.magnitude = test;
-    }
-
-    public Star() {
+    public Star(){
+       super(); 
         this.magnitude = 0.0;
     }
-
-    public Star(String a, double b, double c, double d, double e) {
-        super(a, b, c, d);
-        this.magnitude = e;
+      public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z); 
+        this.magnitude = magnitude;
     }
-
+    public double getMagnitude(){
+        return magnitude;
+    }
+    public void setMagnitude(double newmag){
+      this.magnitude=newmag;
+    }
+  @Override
     public String toString() {
-        return this.name + " shines at the " + String.format("%.3f magnitude", this.magnitude);
+        return String.format("%s shines at the %.3f magnitude", getName(), magnitude);
     }
+     @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; 
+        if (obj == null || getClass() != obj.getClass()) return false;
 
-    public Boolean equals(Star object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-
-        return (this.x == object.x && this.y == object.y && this.z == object.z && this.name.equals(object.name) && this.magnitude == object.magnitude);
+        Star other = (Star) obj;
+        
+        return Double.compare(other.magnitude, magnitude) == 0 &&
+               super.equals(other);
     }
-
+     @Override
     public int hashCode() {
-        return java.util.Objects.hash(name, x, y, z, magnitude);
+        return Objects.hash(super.hashCode(), magnitude);
     }
-}
+}   
