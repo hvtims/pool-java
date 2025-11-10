656019f5ba8106ef9e7ee75594a53f38af04e44e
hvtims <timo.goog03@gmail.com>
2025-11-10 14:42:27 +0100

    adventuremonster

---
+++ AdventureMonster/Monster.java
@@ -0,0 +1,12 @@
+public class Monster extends Character{
+    public Monster(String namee , int maxHealth){
+        super(namee , maxHealth);
+    }
+    @Override
+    public String toString(){
+        if (getCurrentHealth() != 0 ){
+            return getName()+ " is a monster with " +  getCurrentHealth()+ " HP";
+        }
+        return getName() + " is a monster and  is dead";
+    }
+}
