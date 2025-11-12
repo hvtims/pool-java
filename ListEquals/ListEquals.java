2cbe1960c7df4af5b645738f14b86b86c8651738
hvtims <timo.goog03@gmail.com>
2025-11-12 11:05:18 +0100

    listequals

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if (list1 == null || list2 == null){
+        if (list1 == null || list1.length() == 0|| list2 == null|| list2.length() == 0){
             return false;
         }
         if (list1.equals(list2)){
