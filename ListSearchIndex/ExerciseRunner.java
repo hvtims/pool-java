b4608a18da0e74e953bd51368e5d3aec4e82326c
hvtims <timo.goog03@gmail.com>
2025-11-12 11:20:32 +0100

    listsearchindex

--- ListSearchIndex/ExerciseRunner.java
+++ ListSearchIndex/ExerciseRunner.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(ListSearchIndex.findLastIndex(List.of(9, 13, 89, 8, 23, 1, 0, 89), 89));
+        System.out.println(ListSearchIndex.findLastIndex(List.of(28, 4893, 438, 54857, 4374, 4893, 48394), 4893));
         System.out.println(ListSearchIndex.findFirstIndex(List.of(9, 13, 89, 8, 23, 1, 0, 89), 89));
          System.out.println(ListSearchIndex.findAllIndexes(List.of(9, 13, 89, 8, 23, 1, 0, 89), 89).toString());
     }
--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
         for (int i = list.size() -1 ; i > 0 ; i --){
-            if (list.get(i) == value){
+            if (list.get(i).equals(value)){
                 return i;
             }
         }
@@ -17,6 +17,9 @@ public class ListSearchIndex {
         return list.indexOf(value) != -1 ?list.indexOf(value): null ;
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        if (list == null){
+            return null;
+        }
         List<Integer> lista = new ArrayList<>();
         for (int i = 0 ; i < list.size() ; i ++){
             if (list.get(i).equals(value)){
