9142aa09481d9c14be9dc077869f0710332e3153
hvtims <timo.goog03@gmail.com>
2025-11-12 11:00:56 +0100

    listcontains

---
+++ ListContains/ListContains.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class ListContains {
+    public static boolean containsValue(List<Integer> list, Integer value) {
+        if (list.contains(value)){
+            return true;
+        }
+        return false ;
+    }
+}
