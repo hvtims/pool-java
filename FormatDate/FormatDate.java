311d4394e0d539882d7299a38b8c5ecdc282f6a0
hvtims <timo.goog03@gmail.com>
2025-11-17 15:46:46 +0100

    formatdate

---
+++ FormatDate/FormatDate.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class FormatDate {
+
+    public static String formatToFullText(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+            "d MMM 'de l''an' yyyy 'à' HH'h'mm'm et 'ss's'", Locale.FRENCH);
+        return "Le " + dateTime.format(formatter);
+    }
+
+    public static String formatSimple(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d yy", Locale.ITALIAN);
+        return date.format(formatter);
+    }
+
+    public static String formatIso(LocalTime time) {
+        if (time == null) {
+            return null; 
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return time.format(formatter);
+    }
+}
