0bca6529b6815bf727a89e8d35b8dd1d9973b1cc
hvtims <timo.goog03@gmail.com>
2025-11-18 18:18:31 +0100

    singleton

---
+++ Singleton/Excalibur.java
@@ -0,0 +1,20 @@
+public class Excalibur {
+    private String name;
+
+    private static Excalibur INSTANCE;
+
+    private Excalibur(String name) {
+        this.name = name;
+    }
+
+    public static Excalibur getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Excalibur("Sword");
+        }
+        return INSTANCE;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
